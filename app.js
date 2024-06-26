f0305a8c7cd2ee8f2b30f57e7ad7349dc051ed0e
khojiakbar63 <abdulaxatovx7@gmail.com>
2024-06-26 11:52:50 +0500

    top header

--- app.js
+++ app.js
@@ -7,12 +7,22 @@ function $(selector) {
 const menuIcon = $('.menu-icon')
 const sideSanel = $('.side-panel')
 const closeIcon = $('.close-icon')
-
-
+const topHeader = $('.top-header')
+const header = $('header')
 // EVENTS
 menuIcon.addEventListener('click', () => {
     sideSanel.classList.toggle('none')
 });
 closeIcon.addEventListener('click', () => {
     sideSanel.classList.toggle('none');
+})
+window.addEventListener('scroll', () => {
+    if(window.scrollY > 300){
+        topHeader.classList.add('none')
+        header.classList.add('top-0')
+    }else{
+        topHeader.classList.remove('none')
+        header.classList.remove('top-0')
+
+    }
 })
