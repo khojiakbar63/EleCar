1674271288d5b6b7c87d3ab49a5a48ca848382aa
khojiakbar63 <abdulaxatovx7@gmail.com>
2024-06-28 11:38:44 +0500

    loader

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const sideSanel = $('.side-panel')
 const closeIcon = $('.close-icon')
 const topHeader = $('.top-header')
 const header = $('header')
+const list =  $('#list')
 const cards = $('#cards')
 const cards2 = $('#cards2')
 const allBtn = $('#all-btn')
@@ -22,7 +23,26 @@ const luxuryTitle = $('.luxury-title')
 const offerTitle = $('.left-title')
 const formTitle = $('.form-title')
 const formDesc = $('.form-desc')
+const selectMode = $('#modes')
 
+// Modes
+
+const intro = $('#intro')
+const about = $('#about')
+const choose = $('#choose')
+const feature = $('#feature')
+const luxury = $('#luxury')
+const offer = $('#offer')
+const logos = $('#logos')
+const form = $('#form')
+const footer = $('footer')
+const footerA = $$('footer li a')
+const footerH3 = $('footer  h3')
+const loaderWrapper = $('.loader-wrapper')
+// Loader
+window.addEventListener('load', () =>{
+    loaderWrapper.classList.add('none')
+})
 // EVENTS
 menuIcon.addEventListener('click', () => {
     sideSanel.classList.toggle('none')
@@ -104,6 +124,43 @@ select.addEventListener('change', (e) => {
     formDesc.textContent = languages[e.target.value].formDesc
 })
 
+// Modes
+selectMode.addEventListener('change', (e) => {
+    header.style.backgroundColor = modes[e.target.value].backgroundColor,
+    header.style.color = modes[e.target.value].color,
+    list.backgroundColor = modes[e.target.value].backgroundColor,
+    list.color = modes[e.target.value].color
+    intro.style.backgroundColor = modes[e.target.value].backgroundColor
+    intro.style.color = modes[e.target.value].color
+    about.style.backgroundColor = modes[e.target.value].backgroundColor
+    about.style.color = modes[e.target.value].color
+    choose.style.backgroundColor = modes[e.target.value].backgroundColor
+    choose.style.color = modes[e.target.value].color
+    feature.style.backgroundColor = modes[e.target.value].backgroundColor
+    feature.style.color = modes[e.target.value].color
+    luxury.style.backgroundColor = modes[e.target.value].backgroundColor
+    luxury.style.color = modes[e.target.value].color
+    offer.style.backgroundColor = modes[e.target.value].backgroundColor
+    offer.style.color = modes[e.target.value].color
+    logos.style.backgroundColor = modes[e.target.value].backgroundColor
+    logos.style.color = modes[e.target.value].color
+    form.style.backgroundColor = modes[e.target.value].backgroundColor
+    form.style.color = modes[e.target.value].color
+    footer.style.backgroundColor = modes[e.target.value].backgroundColor
+    footer.style.color = modes[e.target.value].color
+
+    aboutTitle.style.backgroundColor = modes[e.target.value].backgroundColor
+    aboutTitle.style.color = modes[e.target.value].color
+
+    featureTitle.style.backgroundColor = modes[e.target.value].backgroundColor
+    featureTitle.style.color = modes[e.target.value].color
+
+    luxuryTitle.style.backgroundColor = modes[e.target.value].backgroundColor
+    luxuryTitle.style.color = modes[e.target.value].color
+    footerA.style.backgroundColor = modes[e.target.value].backgroundColor
+    footerH3.style.color = modes[e.target.value].color
+
+})
 
 
 
---
+++ modes.js
@@ -0,0 +1,12 @@
+const modes = {
+    light: {
+        backgroundColor: '#ffffff',
+        color: '#000000',
+    },
+    dark: {
+        backgroundColor: '#000000',
+        color: '#ffffff'
+    }
+}
+
+
